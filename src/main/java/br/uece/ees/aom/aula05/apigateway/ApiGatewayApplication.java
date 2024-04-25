403066e1afc9ed211f1d0d1fce1df8fd0b7e424f
Anderson Severo <andersonsevero@gmail.com>
2024-04-25 00:04:21 -0300

    Commit inicial

---
+++ src/main/java/br/uece/ees/aom/aula05/apigateway/ApiGatewayApplication.java
@@ -0,0 +1,13 @@
+package br.uece.ees.aom.aula05.apigateway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApiGatewayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ApiGatewayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/br/uece/ees/aom/aula05/apigateway/ApiGatewayApplicationTests.java
@@ -0,0 +1,13 @@
+package br.uece.ees.aom.aula05.apigateway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiGatewayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
